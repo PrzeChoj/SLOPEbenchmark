e226157598a250cfac1a5f51977b0fe1998ac314
PrzeChoj <premysl.choj@gmail.com>
2023-11-05 18:34:32 +0100

    get_lambda for Iza

--- R/singleCluster.R
+++ R/singleCluster.R
@@ -1,7 +1,14 @@
-#' clusters c(0, 5, 100, 95) ===>>> beta = c(0, 0, 0, 0, 0, 100, 100, ..., 100)
+#' clusters = c(0, 5, 100, 95) ===>>> beta = c(0, 0, 0, 0, 0, 100, 100, ..., 100)
 #' @export
 #' @examples
 #' get_data(200, 100, c(0, 5, 100, 95))
+#' l <- get_data(200, 10, c(100, 5, 0, 5))
+#' l <- get_data(200, 15, c(0, 5, -100, 5, 100, 5), sigma = 0, CovarMatrix = "I")
+#'
+#' X <- l$X
+#' Y <- l$Y
+#' n <- l$n
+#' p <- l$p
 get_data <- function(n, p, clusters, sigma = 0, CovarMatrix = "I") {
   stopifnot(length(clusters) %% 2 == 0)
   if (is.character(CovarMatrix)) {
@@ -36,12 +43,32 @@ get_data <- function(n, p, clusters, sigma = 0, CovarMatrix = "I") {
   Y <- X %*% beta + eps
   C <- t(X) %*% X / n
 
-  return(list("X" = X, "Y" = Y, "C" = C, "beta" = beta))
+  return(list("X" = X, "Y" = Y, "C" = C, "beta" = beta, "n" = n, "p" = p))
 }
 
-get_lambda <- function(type = c("B", "K"), X, Y, p, q) {
+#' @export
+get_lambda <- function(type = c("B", "K", "I"), X, Y, p, q, n) {
   if (type == "K") {
     lambda <- sort(abs(t(X) %*% Y), decreasing = TRUE)
+  } else if (type == "I"){ # TODO(Uprość)
+    b_OLS <- solve(t(X) %*% X) %*% t(X) %*% Y
+
+    C <- t(X) %*% X / n
+
+    losowy_pomysl <- TRUE
+    if (losowy_pomysl){
+      b_OLS <- abs(b_OLS)
+    }
+
+    # Tu trzeba pomnożyć macierzą, gdzie każdy wiersz to b_OLS:
+    B <- matrix(rep(b_OLS, p), nrow = p, byrow = TRUE)
+
+    C_falka <- t(B) %*% C %*% B
+
+    lambda <- sort(rowSums(C_falka), decreasing = TRUE)
+
+    # lambda może być ujemna teraz. Trzba, żeby nie:
+    lambda <- ifelse(lambda > 0, lambda, 0)
   } else {
     seq1 <- seq(1:p)
     lambda <- stats::qnorm(1 - q * seq1 / 2 / p)
